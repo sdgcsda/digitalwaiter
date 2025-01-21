272d730597f795889b161983e9f2834becc2bffd
Stndavid <Sdg.caguana@yavirac.edu.ec>
2025-01-20 22:13:08 -0500

    segundo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
   { path: '', redirectTo: '/waiter/dashboard', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'waiter', loadChildren: () => import('./waiter/waiter.module').then(m => m.WaiterModule) },
+
 ];
 
 @NgModule({
