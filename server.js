be4473657a1e965e1319f67a2a2ee937c44d974b
Eerick6 <erick199335@gmail.com>
2025-01-21 16:56:04 -0500

    dist

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/Prototip_digitalW/browser')));
+app.use(express.static(path.join(__dirname, 'dist/prototip_digital-w/browser')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/Prototip_digitalW/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/rototip_digital-w/browser/index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
